8673ee5c376d3f33020d20ac6cac8d37efa0569f
Yves <yvescasio@yahoo.com>
2023-04-11 23:36:29 +0800

    feat: :sparkles: Add Computer choice display, game result display (best of 5), repeatable game

--- script.js
+++ script.js
@@ -5,77 +5,92 @@ function random(number) {
 }
 
 const roshambo = ['rock', 'paper', 'scissors']
-let PlayerChoice;
-let ComputerChoice;
-
-// Plays a round of rock-paper-scissor to computer using .random()
-function playRound(getPlayerChoice, getComputerChoice){
-    getPlayerChoice = prompt('Choose rock, paper, or scissors: ')
-    getComputerChoice = roshambo[random(roshambo.length)].toLowerCase()
-    console.log(`Computer: ${getComputerChoice}`)
-    getPlayerChoice = getPlayerChoice.toLowerCase()
-
-    if(getPlayerChoice == getComputerChoice){
-        return `It's a tie, no one won.`
-    } else if ((getPlayerChoice == 'rock')){
-        if(getComputerChoice == 'paper'){
-            return `You lose, Computer won.`
-        } else if (getComputerChoice == 'scissors'){
-            return `You won, Computer lose.`
-        } else {
-            return
-        }
-    } else if ((getPlayerChoice == 'paper')){
-        if(getComputerChoice == 'scissor'){
-            return `You lose, Computer won.`
-        } else if (getComputerChoice == 'rock'){
-            return `You won, Computer lose.`
-        } else {
-            return
-        }
-    } else if ((getPlayerChoice == 'scissors')){
-        if(getComputerChoice == 'rock'){
-            return `You lose, Computer won.`
-        } else if (getComputerChoice == 'paper'){
-            return `You won, Computer lose.`
-        } else {
-            return
-        }
-    } else {
-        return `Invalid input! Please try again.`
-    }
-}
-
-function game(){
-    const rounds = 5;
-    let i = 1;
-
-    while(i <= rounds){
-        console.log(playRound(PlayerChoice,ComputerChoice))
-        i++
-    }
-}
-
+let playerChoice;
+let computerChoice;
+let playerWins = 0;
+let computerWins = 0;
 
 const rockButton = document.querySelector(".rock-button")
 const paperButton = document.querySelector(".paper-button")
 const scissorsButton = document.querySelector(".scissors-button")
 const playerChoiced = document.querySelector(".player .selected")
+const computerChoiced = document.querySelector(".computer .selected")
+const startButton = document.querySelector(".start-button")
+const resultModal = document.querySelector(".modal-container")
+const resultModalContent = document.querySelector(".modal-content")
+const modalButton = document.querySelector(".modal-button")
 
-rockButton.addEventListener('click', () => {
-    playerChoiced.classList.remove('rock-background', 'paper-background', 'scissors-background')
-    playerChoiced.classList.add('rock-background')
-    PlayerChoice = 'rock'
-})
+const pWins = document.querySelector(".player h1")
+const cWins = document.querySelector(".computer h1")
 
-paperButton.addEventListener('click', () => {
-    playerChoiced.classList.remove('rock-background', 'paper-background', 'scissors-background')
-    playerChoiced.classList.add('paper-background')
-    PlayerChoice = 'paper'
-})
 
-scissorsButton.addEventListener('click', () => {
-    playerChoiced.classList.remove('rock-background', 'paper-background', 'scissors-background')
-    playerChoiced.classList.add('scissors-background')
-    PlayerChoice = 'scissors'
+// Choose the choice of player base by selected button and reflect it in UI using classlist
+function handleChoicePlayer(choice) {
+  playerChoiced.classList.remove(...roshambo.map(c => `${c}-background`));
+  playerChoiced.classList.add(`${choice}-background`);
+  playerChoice = choice;
+}
+
+function handleChoiceComputer(choice) {
+  computerChoiced.classList.remove(...roshambo.map(c => `${c}-background`));
+  computerChoiced.classList.add(`${choice}-background`);
+}
+
+function gameResult() {
+  if(playerWins == 5){
+    resultModal.style.display = 'flex';
+    resultModalContent.textContent = 'You win!'
+  } else if (computerWins == 5) {
+    resultModal.style.display = 'flex';
+    resultModalContent.textContent = 'Computer won!'
+  }
+}
+
+
+
+// Plays a round of rock-paper-scissor to computer using .random()
+function playRound() {
+  
+    console.log(`Start button clicked`)
+    computerChoice = roshambo[Math.floor(Math.random() * roshambo.length)];
+    handleChoiceComputer(computerChoice) // Display the computer choice on the page
+  
+    console.log(`Computer: ${computerChoice}`);
+  
+    if (!roshambo.includes(playerChoice)) {
+      console.log("Invalid Input")
+      return 'Invalid input! Please try again.';
+    }
+  
+    if (playerChoice === computerChoice) {
+      console.log("Tie")
+      return `It's a tie, no one won.`;
+    } else if (
+      (playerChoice === 'rock' && computerChoice === 'scissors') ||
+      (playerChoice === 'paper' && computerChoice === 'rock') ||
+      (playerChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      console.log("Player Won")
+      playerWins = playerWins + 1
+      pWins.textContent = `Player: ${playerWins}`     
+    } else {
+      console.log("Computer Won")
+      computerWins = computerWins + 1
+      cWins.textContent = `Computer: ${computerWins}` 
+    }
+
+    gameResult()
+}
+
+rockButton.addEventListener('click', () => handleChoicePlayer('rock'));
+paperButton.addEventListener('click', () => handleChoicePlayer('paper'));
+scissorsButton.addEventListener('click', () => handleChoicePlayer('scissors'));
+startButton.addEventListener('click', () => playRound());
+
+modalButton.addEventListener('click', ()=> {
+  resultModal.style.display = 'none';
+  pWins.textContent = `Player: ${0}`
+  cWins.textContent = `Computer: ${0}`
+  playerWins = 0
+  computerWins = 0
 })
